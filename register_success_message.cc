2f5eedb18752aae3e811e33fe8f07a58e058c17a
Elisha Lai <elisha.lai@outlook.com>
2017-03-03 13:32:30 -0500

    Add register success message

---
+++ register_success_message.cc
@@ -0,0 +1,11 @@
+#include "register_success_message.h"
+#include "message_types.h"
+
+using namespace std;
+
+// See interface (header file).
+RegisterSuccessMessage::RegisterSuccessMessage()
+  : Message(_MSG_TYPE_REGISTER_SUCCESS) {} //  Constructor
+
+// See interface (header file).
+RegisterSuccessMessage::~RegisterSuccessMessage() {} // Destructor
---
+++ register_success_message.h
@@ -0,0 +1,13 @@
+#ifndef __REGISTER_SUCCESS_MESSAGE_H__
+#define __REGISTER_SUCCESS_MESSAGE_H__
+
+#include "message.h"
+
+// Object definition
+class RegisterSuccessMessage: public Message {
+public:
+  RegisterSuccessMessage();  // Constructor
+  ~RegisterSuccessMessage(); // Destructor
+};
+
+#endif
