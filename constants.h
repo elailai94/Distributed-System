07987404ea7637f1f3bb66350fb5f67e41555630
yfjd01 <y25dai@uwaterloo.ca>
2017-03-04 18:16:12 -0500

    update

--- constants.h
+++ constants.h
@@ -10,18 +10,18 @@ struct server_info {
 	int socket;
 	
 	server_info(string server_identifier, int port) : server_identifier(server_identifier), port(port){}
-} 
+}; 
 
 struct procedure_signature {     
 	string name;
     int *argTypes;
 
 	server_info(string name, int *argTypes) : name(name), argTypes(argTypes){}
-} 
+}; 
 
 struct server_function_info {     
 	server_info si;
 	procedure_signature ps;
 
 	server_info(server_info si, procedure_signature ps): si(si), port(port), socket(socket), ps(ps){}
-} 
+};
