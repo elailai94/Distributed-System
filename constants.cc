26744f6b1aba9d4a9a5704e1ad07030d92911bfd
yfjd01 <y25dai@uwaterloo.ca>
2017-03-05 16:26:13 -0500

    message

--- constants.cc
+++ constants.cc
@@ -17,8 +17,7 @@ procedure_signature::procedure_signature(const struct procedure_signature & r) {
 server_function_info::server_function_info(struct server_info *si, struct procedure_signature *ps) : si(si), ps(ps){}
 
 bool operator == (const server_function_info &l, const server_function_info &r) {
-    //return (l.si->server_identifier == r.si->server_identifier && l.si->port == r.si->port && (!(*(l.ps) < *(r.ps)) && !(*(r.ps) < *(l.ps))));
-	return (l.si->server_identifier == r.si->server_identifier && l.si->port == r.si->port);
+    return (l.si->server_identifier == r.si->server_identifier && l.si->port == r.si->port);
 }
 
 bool operator == (const server_info &l, const server_function_info &r) {
