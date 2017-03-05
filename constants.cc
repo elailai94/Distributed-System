fd910dc940c2b57427e551c5a019f82df39f2094
yfjd01 <y25dai@uwaterloo.ca>
2017-03-05 16:06:18 -0500

    changes

--- constants.cc
+++ constants.cc
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+
+
+server_info::server_info(string server_identifier, int port, int socket): server_identifier(server_identifier), port(port), socket(socket){}
+
+procedure_signature::procedure_signature(string name, int *argTypes): name(name), argTypes(argTypes){}
+
+procedure_signature::procedure_signature(const struct procedure_signature & r) {
+    name = r.name;
+    argTypes = r.argTypes;
+}
+
+server_function_info::server_function_info(struct server_info *si, struct procedure_signature *ps) : si(si), ps(ps){}
+
 bool operator == (const server_function_info &l, const server_function_info &r) {
     //return (l.si->server_identifier == r.si->server_identifier && l.si->port == r.si->port && (!(*(l.ps) < *(r.ps)) && !(*(r.ps) < *(l.ps))));
 	return (l.si->server_identifier == r.si->server_identifier && l.si->port == r.si->port);
--- constants.h
+++ constants.h
@@ -18,29 +18,23 @@ struct server_info {
 	int port;
 	int socket;
 
-	server_info(string server_identifier, int port, int socket)
-	  : server_identifier(server_identifier), port(port), socket(socket){}
+	server_info(string server_identifier, int port, int socket);
 };
 
 struct procedure_signature {
 	string name;
     int *argTypes;
 
-	procedure_signature(string name, int *argTypes)
-	  : name(name), argTypes(argTypes){}
+	procedure_signature(string name, int *argTypes);
 
-    procedure_signature(const struct procedure_signature & r) {
-        name = r.name;
-        argTypes = r.argTypes;
-    }
+    procedure_signature(const struct procedure_signature & r);
 };
 
 struct server_function_info {
 	struct server_info *si;
 	struct procedure_signature *ps;
 
-	server_function_info(struct server_info *si, struct procedure_signature *ps)
-	  : si(si), ps(ps){}
+	server_function_info(struct server_info *si, struct procedure_signature *ps);
 };
 
 bool operator == (const server_info &l, const struct server_function_info  &r);
