afe975f3cade2203cd011c47d861536d2e7567d1
yfjd01 <y25dai@uwaterloo.ca>
2017-03-04 19:56:11 -0500

    update

--- binder.cc
+++ binder.cc
@@ -105,9 +105,9 @@ USE ROUND ROBIN TO ACCESS THE CORRECT SERVER/FUNCTION FOR THE CLIENT
 int location_request_handler(LocRequestMessage * message, int sock){
  
   bool exist = false; 
-	for (list<server_function_info *>::iterator it = roundRobinList->begin(); it != roundRobinList->end(); it++){
+	for (list<server_function_info *>::iterator it = roundRobinList.begin(); it != roundRobinList.end(); it++){
 		//If the name are the same
-    if(it->ps->name == message->getName() && compareArr(it->ps->argTypes, message->getArgTypes() )){ 
+    if(it.ps->name == message->getName() && compareArr(it.ps->argTypes, message->getArgTypes() )){ 
 		  //When we have identified the correct procedure_signature use round robin and move that service to the end
 		  roundRobinList.splice(roundRobinList.end(), roundRobinList, it);
       exist = true;
