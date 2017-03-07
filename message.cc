5d47430cbc608286d895a3837aa27ccb2fdde0fe
Elisha Lai <elisha.lai@outlook.com>
2017-03-06 22:45:56 -0500

    fix reference to pointer issue

--- message.cc
+++ message.cc
@@ -5,3 +5,9 @@ Message::Message() {} // Constructor
 
 // See interface (header file).
 Message::~Message() {} // Destructor
+
+// See interface (header file).
+int Message::receive(int dataTransferSocket, Message *&parsedMessage,
+  unsigned int length) {
+  return 0;  
+}
