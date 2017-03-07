e331ccfb6115c013b2be857bc43df3f5ae2fa19f
Elisha Lai <elisha.lai@outlook.com>
2017-03-07 16:20:51 -0500

    work

--- test/server.cc
+++ test/server.cc
@@ -24,7 +24,7 @@ string getServerAddress() {
 int getServerPort(int welcomeSocket) {
    struct sockaddr_in serverAddress;
    socklen_t serverAddressLength = sizeof(serverAddress);
-   
+
    int result = getsockname(welcomeSocket, (struct sockaddr*) &serverAddress,
       &serverAddressLength);
    if (result < 0) {
@@ -51,7 +51,7 @@ int setUpWelcomeSocket() {
    serverAddressHints.ai_family = AF_INET;
    serverAddressHints.ai_socktype = SOCK_STREAM;
    serverAddressHints.ai_flags = AI_PASSIVE;
-   
+
    // Gets the server's IP address and the next available port
    int result = getaddrinfo(NULL, "0", &serverAddressHints,
       &serverAddressResults);
@@ -85,60 +85,53 @@ int setUpWelcomeSocket() {
 } // setUpWelcomeSocket
 
 int main() {
-   // Sets up the welcome socket, adds it to the all sockets set and
-   // sets it as the maximum socket so far
-   int welcomeSocket = setUpWelcomeSocket();
+  // Sets up the welcome socket, adds it to the all sockets set and
+  // sets it as the maximum socket so far
+  int welcomeSocket = setUpWelcomeSocket();
 
-   while (true) {            
-      struct sockaddr clientAddress;
-            socklen_t clientAddressLength = sizeof(clientAddress);
+  while (true) {
+    struct sockaddr clientAddress;
+    socklen_t clientAddressLength = sizeof(clientAddress);
 
-            // Creates the connection socket when a connection is made
-            // to the welcome socket
-            int connectionSocket = accept(welcomeSocket, &clientAddress,
-               &clientAddressLength);
-            if (connectionSocket < 0) {
-               continue;
-            } // if
+    // Creates the connection socket when a connection is made
+    // to the welcome socket
+    int connectionSocket = accept(welcomeSocket, &clientAddress,
+      &clientAddressLength);
+      if (connectionSocket < 0) {
+        continue;
+      } // if
 
-            Segment *parsedSegment = 0;
-            int result = 0;
-            result = Segment::receive(connectionSocket, parsedSegment);
-            if (result < 0) {
-               // Closes the connection socket and removes it from the
-               // all sockets set
-               close(connectionSocket);
-            } // if
+    Segment *parsedSegment = 0;
+    int result = 0;
+    result = Segment::receive(connectionSocket, parsedSegment);
+    if (result < 0) {
+      // Closes the connection socket and removes it from the
+      // all sockets set
+      close(connectionSocket);
+    } // if
 
-            if (parsedSegment != 0) {
-               Message *msg = parsedSegment->getMessage();
-              	  cout << "Segment type is: " << parsedSegment->getType() << endl;
-		if (msg != 0) {
-                  cout << "Message isn't null...." << endl;
-                  RegisterRequestMessage *rrm = dynamic_cast<RegisterRequestMessage *>(msg);
-                	 
-		 if (rrm != 0) {
-                     cout << "RRM isn't null..." << endl;
+    if (parsedSegment != 0) {
+      Message *msg = parsedSegment->getMessage();
+      cout << "Segment length is: " << parsedSegment->getLength() << endl;
+      cout << "Segment type is: " << parsedSegment->getType() << endl;
+		  if (msg != 0) {
+        RegisterRequestMessage *rrm = dynamic_cast<RegisterRequestMessage *>(msg);
+
+		    if (rrm != 0) {
 		     cout << "Server Identifier: " << rrm->getServerIdentifier() << endl;
-                	
-		     int tempInt = rrm->getPort(); 
+
+		     unsigned int tempInt = rrm->getPort();
 		     cout << "Port: " << tempInt << endl;
 
-                     string rrmName  = rrm->getName();
+         string rrmName  = rrm->getName();
 		     int strLen = rrmName.length();
 
 		     cout << "Name: " << rrmName << endl;
-                     cout << "Name Length: " << strLen << endl;
+         cout << "Name Length: " << strLen << endl;
 		     cout << "ArgTypes: " << *(rrm->getArgTypes()) << ", " << *(rrm->getArgTypes()+1) << ", " << *(rrm->getArgTypes()+2)  << ", " << *(rrm->getArgTypes()+3) << endl;
-		}
-               }
-               //cout << parsedSegment->getLength() << endl;
-               //cout << parsedSegment->getType() << endl;
-            }
-            //RegisterRequestMessage *msg = dynamic_cast<RegisterRequestMessage *>(parsedSegment->getMessage());
-
-            //cout << msg->getLength() << endl;
-            //cout << msg->getPort() << endl;
+		   }
+     }
+    }
    } // while
 
    // Closes the welcome socket
