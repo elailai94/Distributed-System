4f4c59608351cd70e18c48e3710d50ae3c720490
yfjd01 <y25dai@uwaterloo.ca>
2017-03-08 21:42:09 -0500

    changes

--- binder.cc
+++ binder.cc
@@ -141,7 +141,7 @@ void location_request_handler(LocRequestMessage * message, int sock){
       cout << "server_identifier: "<< (*it)->si->server_identifier << endl;
       cout << "port: " << (*it)->si->port<< endl;
       
-      LocSuccessMessage locSuccessMsg = LocSuccessMessage((*it)->si->server_identifier, (*it)->si->port);
+      LocSuccessMessage locSuccessMsg = LocSuccessMessage((*it)->si->server_identifier.c_str(), (*it)->si->port);
       Segment locSuccessSeg = Segment(locSuccessSeg.getLength(), MSG_TYPE_LOC_SUCCESS, &locSuccessMsg);
       locSuccessSeg.send(sock);
 
