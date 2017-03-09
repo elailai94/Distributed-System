bd8aaa64be8a85aa43d16734cd6073920cd08928
Elisha Lai <elisha.lai@outlook.com>
2017-03-09 15:20:53 -0500

    add helper functions

--- helper_functions.h
+++ helper_functions.h
@@ -10,10 +10,26 @@ unsigned int countNumOfArgTypes(int* argTypes);
 
 int *copyArgTypes(int argTypes[]);
 
-// Converts an unsigned number to a string
+/* Type conversion functions */
+// Converts an unsigned integer to a string
 std::string toString(unsigned int number);
 
-// Converts a string to an unsigned number
+// Converts a string to an unsigned integer
 unsigned int toUnsignedInteger(std::string text);
+
+// Converts a character array to a short
+short toShort(char *array);
+
+// Converts a character array to an integer
+int toInt(char *array);
+
+// Converts a character array to a long
+long toLong(char *array);
+
+// Converts a character array to a double
+double toDouble(char *array);
+
+// Converts a character array to a float
+float toFloat(char *array);
 
 #endif
