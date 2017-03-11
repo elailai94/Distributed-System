cb8a1c9705d33156529eaa9cf3a5e9702186266d
yfjd01 <y25dai@uwaterloo.ca>
2017-03-11 14:14:44 -0500

    msg

--- rpc.cc
+++ rpc.cc
@@ -247,7 +247,7 @@ int rpcCall(char *name, int *argTypes, void **args) {
   Segment exeReqSeg = Segment(exeReqMsg.getLength(), MSG_TYPE_EXECUTE_REQUEST, &exeReqMsg);
   int status2 =  exeReqSeg.send(serverSocket);
 
-  cout << "Status of exeRegMsg send: " << status2 << end:;
+  cout << "Status of exeRegMsg send: " << status2 << endl;
 
   int returnVal = 0;
 
