c3d8ee87baf3ff04ebd638adaa4ee62db28a164d
yfjd01 <y25dai@uwaterloo.ca>
2017-03-12 14:39:13 -0400

    changes

--- client.c
+++ client.c
@@ -133,10 +133,8 @@ int main() {
   }
 
 
-  //int s3 = rpcCall("f3", argTypes3, args3);
+  int s3 = rpcCall("f3", argTypes3, args3);
   /* test the return of f3 */
-  
-  /*
   printf(
     "\nEXPECTED return of f3 is: 110 109 108 107 106 105 104 103 102 101 11\n"
   );
@@ -152,11 +150,9 @@ int main() {
   else {
     printf("Error: %d\n", s3);
   }
-  */
-  
-  //int s4 = rpcCall("f4", argTypes4, args4);
+
+  int s4 = rpcCall("f4", argTypes4, args4);
   /* test the return of f4 */
-  /*
   printf("\ncalling f4 to print an non existed file on the server");
   printf("\nEXPECTED return of f4: some integer other than 0");
   printf("\nACTUAL return of f4: %d\n", s4);
