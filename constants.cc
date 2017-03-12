2aca4bbad821719d7f797ce9bd2e28ef3912b2d1
yfjd01 <y25dai@uwaterloo.ca>
2017-03-11 21:06:49 -0500

    msg

--- constants.cc
+++ constants.cc
@@ -18,50 +18,38 @@ procedure_signature::procedure_signature(const struct procedure_signature & r) {
 
 server_function_info::server_function_info(struct server_info *si, struct procedure_signature *ps) : si(si), ps(ps){}
 
-bool ps_compare::operator() (const procedure_signature& l, const procedure_signature& r) const{
+bool ps_compare::operator() (const procedure_signature& lhs, const procedure_signature& rhs) const{
 
-if(l.name == r.name)
-    {
-        int i = 0;
-        int lArgs = l.argTypes[i];
-        int rArgs = r.argTypes[i];
+  if(lhs.name == rhs.name){
+    int i = 0;
+    int lArgs = lhs.argTypes[i];
+    int rArgs = rhs.argTypes[i];
 
-        // structs with less args are smaller
-        // structs with the same args are the same
-        // structs with the same number args,
-        // but different arg values,
-        // the one with the smaller value is smaller
-        while(lArgs!= 0 && rArgs != 0)
-        {
-            int lArraySize = lArgs & 0x1111;
-            int rArraySize = rArgs & 0x1111;
+    while(lArgs!= 0 && rArgs != 0) {
+      int lArraySize = lArgs & 0x1111;
+      int rArraySize = rArgs & 0x1111;
 
-            if (((lArgs & 0xFFFF0000) != (rArgs & 0xFFFF0000)) || (lArraySize == 0 && rArraySize != 0) || (lArraySize != 0 && rArraySize == 0))
-            {
-                return lArgs < rArgs;
-            }
-            i++;
-            lArgs = l.argTypes[i];
-            rArgs = r.argTypes[i];
-        }
+      if (((lArgs & 0xFFFF0000) != (rArgs & 0xFFFF0000)) 
+        || (lArraySize == 0 && rArraySize != 0) 
+        || (lArraySize != 0 && rArraySize == 0)){
+        
+        return lArgs < rArgs;
+      }
 
-        if(rArgs == 0) // || lArgs == 0
-        {
-            // l is longer, so l > r
-            // or same length with same parameters, so l == r, which means l < r is false
-            return false;
-        }
-        else
-        {
-            // r is longer, so l < r
-            return true;
-        }
+      i++;
+   
+      lArgs = lhs.argTypes[i];
+      rArgs = rhs.argTypes[i];
     }
-    else
-    {
-        // c > 0 if l > r
-        // c < 0 if l < r
-        return (l.name < r.name);
+
+    if(rArgs == 0){
+        return false;    
+    } else {
+        return true;
     }
+
+  } else {
+      return (lhs.name < rhs.name);
+  }
 }
 
