10de0bc5f6bd9d91f7039d3c151c002539a58a37
Elisha Lai <elisha.lai@outlook.com>
2017-03-15 13:31:04 -0400

    work

--- ezCompile.bash
+++ ezCompile.bash
@@ -1,7 +1,5 @@
-make clean && 
-make && 
-g++ -L. client.o -lrpc -lpthread -o client && 
-g++ -L. clientA.o -lrpc -lpthread -o clientA && 
-g++ -L. server_functions.o server_function_skels.o server.o -lrpc -lpthread -o server && 
+make clean &&
+make test &&
+g++ -L. clientA.o -lrpc -lpthread -o clientA &&
 g++ -L. server_functions.o server_function_skels.o serverA.o -lrpc -lpthread  -o serverA &&
 g++ -L. server_functions.o server_function_skels.o serverB.o -lrpc -lpthread -o serverB
